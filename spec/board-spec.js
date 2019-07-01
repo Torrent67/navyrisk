9faabcd1234d979bb13c9c9c4df8c632baac4e57
Marqis Ramos and Trevor Teigen <marqis@>
2019-07-01 15:24:02 -0700

    introduce logic for creating board

--- spec/board-spec.js
+++ spec/board-spec.js
@@ -2,15 +2,19 @@ import { Board } from "./../src/board.js"
 
 describe('Board TEST', function() {
 
-  it('should create a board object with specs outlined in constructor', function(){
-    let board = new Board (2, 4);
-    expect(board.size).toEqual(20);
-    expect(board.islands).toEqual(75);
-  })
-  // it('should modify the board based on number of players', function (){
-  //
-  // })
+let board = new Board (2, 1);
 
+  it('should create a board object with specs outlined in constructor', function(){
+    expect(board.size).toEqual(20);
+    expect(board.islands).toEqual(5);
+  })
+  it('should generate an array of 5 random island shapes', function (){
+    board.makeIslandList();
+    console.log(this.shapes);
+    let layout = board.createIslands();
+    console.log(layout);
+    expect(layout.length).toEqual(5);
+  })
 
 
 })
--- src/board.js
+++ src/board.js
@@ -2,13 +2,13 @@
 export class Board {
   constructor(playerCount, gameMode){
     this.size = 20;
-    this.islands = 30;
+    this.islands = 5;
     this.victoryPoint = playerCount+1;
     this.gameSize = gameMode; //will be used as a modifier for pool and board size
     this.costPool = 400
   }
 
-  makeBoard() {
+  makeBoard() { // makes the board
     let board = document.getElementsByClassName('gameBoard')[0];
     board.innerHTML = '';
     let counter = 0;
@@ -33,10 +33,54 @@ export class Board {
     }
   }
 
-  makeIslands() {
+  makeIslandList() { //creates an array of various island options
+    this.shapes = [];
+    let other = [[0, 0], [0,1], [0,2],[1,1]];
+    let line = [[0, 0], [0, 1]];
+    let l = [[0,0],[1,0],[0,1]];
+    // let z = [[0,1], [1,1], [1,0], [2,0]];
+    let diag = [[0,0], [1,1], [2,2]];
+    let s = [[0,0], [0,1], [1,1], [1,2]]
+    this.shapes.push(line);
+    this.shapes.push(other);
+    this.shapes.push(l);
+    // shapes.push(z);
+    this.shapes.push(diag);
+    this.shapes.push(s)
+  }
+  createIslands(){ //chooses island type and specifies its location on board
+    let currentIslands = [];
+    for (let i = 0; i < 5; i++) {
+      let randomIsland = this.shapes[Math.floor(Math.random()*this.shapes.length)];
+      currentIslands.push(randomIsland);
+    }
+    console.log("in function: " +currentIslands);
+    console.log("in function: "+currentIslands[1]);
+    return currentIslands;
+  }
+  placeIslands (currentIslands) {
+    let location1 = [5,5];
+    let location2 =[15,5];
+    let location3 = [5,15];
+    let location4 = [15,15];
+    let location5 = [Math.random()*(14-6),Math.random()*(14-6)];
+
+    for (let i=1; i < 5; i++) {
+      let island = currentIslands[i];
+      let spot = document.querySelector('[data-x="'+location+i[0]'"][data-y="'+location+i[1]+'"]')
+    }
+
+
+
 
   }
+
+
+
+
+
 }
+
 
 
 
