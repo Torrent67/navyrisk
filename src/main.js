b3a6b53ec0b92f35301c52133df8e470f4a479ce
Marqis Ramos and Trevor Teigen <marqis@>
2019-07-02 11:58:22 -0700

    added ships logic, working on ship position

--- src/main.js
+++ src/main.js
@@ -5,23 +5,41 @@ import './styles.css';
 import { Board } from './board.js';
 import { Ship } from './ships.js';
 
-function attachListeners(ship) {
+function attachListeners() {
+  let ship;
+  $('.ship').on("click", function() {
+    if (this.id === "destroyer") {
+      ship = shipyard[0];
+    }
+    if (this.id === "cruiser") {
+      ship = shipyard[1];
+    }
+    if (this.id === "battlecruiser") {
+      ship = shipyard[2];
+    }
+    if (this.id === "battleship") {
+      ship = shipyard[3];
+    }
+    if (this.id === "carrier") {
+      ship = shipyard[4];
+    }
+  })
   $('.block').on("click", function() {
-    let x = this.dataset.x;
-    let y = this.dataset.y;
-    let occupied = this.dataset.state;
+    let x = Math.abs(parseInt(this.dataset.x));
+    let y = Math.abs(parseInt(this.dataset.y));
+    let occupied = parseInt(this.dataset.state);
     ship.placeShip(x,y, occupied);
 })
 }
 
 let shipyard=[];
 function shipInit() {
-  let carrier = new Ship (50, 2, 4, 1, 10,"" );
-  let battleship = new Ship (40, 5, 4, 1, 6,"" );
-  let battlecruiser = new Ship (20, 3, 3, 2, 4,"" );
-  let cruiser = new Ship (10, 2, 2, 3, 2,"" );
-  let destroyer = new Ship (5, 1, 1, 3, 1, "");
-  shipyard.push(carrier, battleship, battlecruiser, cruiser, destroyer)
+  let destroyer = new Ship ("destroyer", 5, 1, 1, 3, 1);
+  let cruiser = new Ship ("cruiser", 10, 2, 2, 3, 2);
+  let battlecruiser = new Ship ("battlercruiser", 20, 3, 3, 2, 4);
+  let battleship = new Ship ("battleship", 40, 5, 4, 1, 6);
+  let carrier = new Ship ("carrier", 50, 2, 4, 1, 10);
+  shipyard.push(destroyer, cruiser, battlecruiser, battleship, carrier)
 }
 
 $(function() {
@@ -30,6 +48,5 @@ $(function() {
   board.makeBoard();
   board.placeIslands();
   let ship = shipyard[0];
-  attachListeners(ship);
-
+  attachListeners();
 });
--- src/ships.js
+++ src/ships.js
@@ -3,24 +3,39 @@
 
 export class Ship {
 
-  constructor (cost, atk, def, ms, range, img, team){
+  constructor (name, cost, atk, def, ms, range, team){
+    this.name = name;
     this.cost = cost;
     this.attack = atk;
     this.defense = def;
     this.move = ms;
     this.range = range;
-    this.image = 'url("'+img+'")';
-    this.team = team;
+    this.team = 1;
   }
 
   placeShip (x,y,occupied) {
     this.newPosition = [x,y,occupied];
+    console.log(this.newPosition);
+    console.log(this.position);
     // console.log(this.newPosition);
 
     if (this.newPosition[2] !== 0) {
-
+      if (this.newPosition[2] === this.team) {
+        //own ship
+      } else if (this.newPosition[2] === 5) {
+        alert("Island captain! She'll run aground!")
+      } else {
+        //attack ship
+      }
     }
-
-
+    if (!this.position) {
+      this.position = this.newPosition;
+      this.position[2]= this.team;
+    } else if (Math.abs(y-this.position[1]) <= this.move && Math.abs(x-this.position[0]) <= this.move ){
+      this.position = this.newPosition;
+      this.position[2]= this.team;
+    } else {
+      alert("TOO FAR")
+    }
   }
 }
